ea7d9f0836c3a416b0cd8648a767baf2c6ffb045
kurgon <kurgon@proton.me>
2023-05-28 14:38:10 +0000

    hypertype base code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+use colored::*;
+mod init;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    env_logger::init();
+    println!("{}", "⚡HYPERTYPE ⚡".bold().yellow());
+    println!("{} {}", "Author:".green(), "Kurgon".purple());
+    println!("{} {}\n", "Version".green(), "0.1.0".purple());
+
+    init::init().await?;
+
+    Ok(())
 }
