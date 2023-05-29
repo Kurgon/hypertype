1e6850cbbcf9d0a738f2e1df5eba278c97f0e010
kurgon <kurgon@proton.me>
2023-05-29 13:53:27 +0000

    writing's not that easy, but

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,8 @@ mod init;
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     env_logger::init();
     println!("{}", "⚡HYPERTYPE ⚡".bold().yellow());
-    println!("{} {}", "Author:".green(), "Kurgon".purple());
-    println!("{} {}\n", "Version".green(), "0.1.0".purple());
+    println!("{}    {}", "Author:".green(), "Kurgon".purple());
+    println!("{}   {}\n", "Version:".green(), "0.1.0".purple());
 
     init::init().await?;
 
